b7791c2e991d54b21f9ddb414e92d39631ef9da3
Raktimava Bhattacharyya <raktim.bhatta29@gmail.com>
2026-04-22 02:13:21 +0530

    Deployment

--- frontend/src/components/Upload.jsx
+++ frontend/src/components/Upload.jsx
@@ -6,7 +6,7 @@ function Upload() {
   const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
-  const BACKEND_URL = "https://flashcard-engine-5zbb.onrender.com/";
+  const BACKEND_URL = "https://flashcard-engine-5zbb.onrender.com";
 
   const handleUpload = async () => {
     if (!file) return;
